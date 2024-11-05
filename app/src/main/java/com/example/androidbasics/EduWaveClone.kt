71e54f636003fcc579dad47629f01e1ce4802477
Ishaant Kumar Singh <ishaant97@gmail.com>
2024-11-05 13:15:56 +0530

    Edu Rev clone

---
+++ app/src/main/java/com/example/androidbasics/EduWaveClone.kt
@@ -0,0 +1,16 @@
+package com.example.androidbasics
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EduWaveClone : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edu_wave_clone)
+
+    }
+}
